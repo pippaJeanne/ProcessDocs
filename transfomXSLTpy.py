fb974e8addbd6ad7cead9aed6323533366d289e1
pippaJeanne <yhranlo@gmail.com>
2025-02-20 18:43:40 -0500

    some changes

--- transfomXSLTpy.py
+++ transfomXSLTpy.py
@@ -4,7 +4,9 @@ from saxonche import PySaxonProcessor
 inputpath = "input/"
 xsltfile = "Modernisation/modernisation.xslt" #path to xslt file
 outpath = "output/"
-doc = "input/M-Falais24_06_44.xml" #path to input xml file
+doc = "input/0M-Falais24_06_44.xml" #path to input xml file
+slices = doc.split("/")
+name = slices[-1]
 # Create a Saxon processor instance
 with PySaxonProcessor(license=False) as proc:
    xsltproc = proc.new_xslt30_processor()
@@ -12,7 +14,7 @@ with PySaxonProcessor(license=False) as proc:
    executable = xsltproc.compile_stylesheet(stylesheet_file=xsltfile)
    output = executable.transform_to_string(xdm_node=document)
    #write name of output file
-   outfile = open(f"{outpath}modernisation/M-Falais24_06_44.xml", 'w', encoding='utf8') 
+   outfile = open(f"{outpath}modernisation/{name}", 'w', encoding='utf8') 
    outfile.write(output)
 print("Done!")
 #for dirpath, dirnames, filenames in os.walk(inputpath):
